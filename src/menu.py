5effcb2a6f03669bbfef1584a4b396d7168e586d
Marine14 <marine.favier@hotmail.fr>
2020-02-03 17:45:01 +0100

    feature menu les boutons et les textes sont bien centrés

--- src/menu.py
+++ src/menu.py
@@ -25,7 +25,7 @@ while running:
         ##titre du menu
         font = pygame.font.SysFont('Verdana', 80)
         Text_1 = font.render("Menu", 1, (255, 255, 255))
-        Text_1_pos = (constant.WIDTH//2-Text_1.get_rect().width//2, 95)
+        Text_1_pos = (constant.WIDTH//2-Text_1.get_rect().width//2, 100-Text_1.get_rect().height//2)
         screen.blit(Text_1, Text_1_pos)
 
         ##affichage des bouttons
@@ -46,7 +46,7 @@ while running:
             pygame.Rect(constant.WIDTH//2-150, 300, 300, 50))
         # Chargement et collage du text_2
         Text_2 = font.render("Nom des joueurs", 1, (10, 10, 10))
-        Text_2_pos = (constant.WIDTH//2-Text_1.get_rect().width//2, 310)
+        Text_2_pos = (constant.WIDTH//2-Text_2.get_rect().width//2, 310)
         screen.blit(Text_2, Text_2_pos)
 
         ##Boutton 3
@@ -56,7 +56,7 @@ while running:
             pygame.Rect(constant.WIDTH//2-150, 400, 300, 50))
         # Chargement et collage du text_3
         Text_3 = font.render("Paramètres", 1, (10, 10, 10))
-        Text_3_pos = (constant.WIDTH//2-Text_1.get_rect().width//2, 410)
+        Text_3_pos = (constant.WIDTH//2-Text_3.get_rect().width//2, 410)
         screen.blit(Text_3, Text_3_pos)
 
         ##Boutton 4
@@ -66,7 +66,7 @@ while running:
             pygame.Rect(constant.WIDTH//2-150, 500, 300, 50))
         # Chargement et collage du text_4
         Text_4 = font.render("Crédits", 1, (10, 10, 10))
-        Text_4_pos = (constant.WIDTH//2-Text_1.get_rect().width//2, 510)
+        Text_4_pos = (constant.WIDTH//2-Text_4.get_rect().width//2, 510)
         screen.blit(Text_4, Text_4_pos)
 
         # Rafraichissement
