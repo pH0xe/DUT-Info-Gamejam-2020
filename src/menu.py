fa85094754b0bd22e7532c019a749a3f8cbe4ed3
Marine14 <marine.favier@hotmail.fr>
2020-02-04 08:22:59 +0100

    feature menu suppression des majuscules des var

--- src/menu.py
+++ src/menu.py
@@ -24,9 +24,9 @@ while running:
 
         ##titre du menu
         font = pygame.font.SysFont('Verdana', 80)
-        Text_1 = font.render("Menu", 1, (255, 255, 255))
-        Text_1_pos = (constant.WIDTH//2-Text_1.get_rect().width//2, 100-Text_1.get_rect().height//2)
-        screen.blit(Text_1, Text_1_pos)
+        text_1 = font.render("Menu", 1, (255, 255, 255))
+        text_1_pos = (constant.WIDTH//2-text_1.get_rect().width//2, 100-text_1.get_rect().height//2)
+        screen.blit(text_1, text_1_pos)
 
         ##affichage des bouttons
         rect = pygame.draw.rect(
@@ -35,9 +35,9 @@ while running:
             pygame.Rect(constant.WIDTH//2-150, 200, 300, 50))
         # Chargement et collage du text_1
         font = pygame.font.SysFont('Helvetic', 50)
-        Text_1 = font.render("Jouer", 1, (10, 10, 10))
-        Text_1_pos = (constant.WIDTH//2-Text_1.get_rect().width//2, 210)
-        screen.blit(Text_1, Text_1_pos)
+        text_1 = font.render("Jouer", 1, (10, 10, 10))
+        text_1_pos = (constant.WIDTH//2-text_1.get_rect().width//2, 210)
+        screen.blit(text_1, text_1_pos)
 
         ##Boutton 2
         rect = pygame.draw.rect(
@@ -45,9 +45,9 @@ while running:
             (255, 0, 0),
             pygame.Rect(constant.WIDTH//2-150, 300, 300, 50))
         # Chargement et collage du text_2
-        Text_2 = font.render("Nom des joueurs", 1, (10, 10, 10))
-        Text_2_pos = (constant.WIDTH//2-Text_2.get_rect().width//2, 310)
-        screen.blit(Text_2, Text_2_pos)
+        text_2 = font.render("Nom des joueurs", 1, (10, 10, 10))
+        text_2_pos = (constant.WIDTH//2-text_2.get_rect().width//2, 310)
+        screen.blit(text_2, text_2_pos)
 
         ##Boutton 3
         rect = pygame.draw.rect(
@@ -55,9 +55,9 @@ while running:
             (255, 0, 0),
             pygame.Rect(constant.WIDTH//2-150, 400, 300, 50))
         # Chargement et collage du text_3
-        Text_3 = font.render("Paramètres", 1, (10, 10, 10))
-        Text_3_pos = (constant.WIDTH//2-Text_3.get_rect().width//2, 410)
-        screen.blit(Text_3, Text_3_pos)
+        text_3 = font.render("Paramètres", 1, (10, 10, 10))
+        text_3_pos = (constant.WIDTH//2-text_3.get_rect().width//2, 410)
+        screen.blit(text_3, text_3_pos)
 
         ##Boutton 4
         rect = pygame.draw.rect(
@@ -65,9 +65,9 @@ while running:
             (255, 0, 0),
             pygame.Rect(constant.WIDTH//2-150, 500, 300, 50))
         # Chargement et collage du text_4
-        Text_4 = font.render("Crédits", 1, (10, 10, 10))
-        Text_4_pos = (constant.WIDTH//2-Text_4.get_rect().width//2, 510)
-        screen.blit(Text_4, Text_4_pos)
+        text_4 = font.render("Crédits", 1, (10, 10, 10))
+        text_4_pos = (constant.WIDTH//2-text_4.get_rect().width//2, 510)
+        screen.blit(text_4, text_4_pos)
 
         # Rafraichissement
         pygame.display.flip()
