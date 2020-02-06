a4f705e8f957cd04abd166c0dd963a763025ac34
pH0xe <reig.julien@free.fr>
2020-02-06 13:42:09 +0100

    fix erreur clique bouton

--- src/NamePlayer.py
+++ src/NamePlayer.py
@@ -59,12 +59,10 @@ class NamePlayer:
                     windowstate.playerName = False
                     pygame.quit()
 
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    pos = pygame.mouse.get_pos()
-                    if menu.collidepoint(pos):
-                        windowstate.menu = True
-                        windowstate.playerName = False
-                        running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN and menu.collidepoint(pygame.mouse.get_pos()):
+                    windowstate.menu = True
+                    windowstate.playerName = False
+                    running = False
 
                 elif event.type == pygame.KEYDOWN and event.key == pygame.K_TAB:
                     if self.inputBox1.active:
@@ -74,7 +72,7 @@ class NamePlayer:
                         self.inputBox2.active = False
                         self.inputBox1.active = True
 
-                elif (event.type == pygame.MOUSEBUTTONDOWN and play.collidepoint(pos)) or (event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN):
+                elif (event.type == pygame.MOUSEBUTTONDOWN and play.collidepoint(pygame.mouse.get_pos())) or (event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN):
                         if self.inputBox1.text != '' and self.inputBox2.text != '':
                             if self.inputBox1.text == 'blanchon' or self.inputBox1.text == 'Blanchon' or self.inputBox1.text == 'Herve' or self.inputBox1.text == 'Hervé':
                                 windowstate.isBlanchon1 = True
